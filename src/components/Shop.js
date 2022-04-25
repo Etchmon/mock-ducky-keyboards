19cad8416d00e797afe1e4941ce8b05eb32edc83
James Etchells <etch.rush@gmail.com>
2022-04-25 14:22:26 -0700

    Add grid css to shop display Add product css, overlay, and hover effects

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,7 @@
-
+import allProducts from "../data/AllProducts";
 
 function Shop(props) {
-
+    console.log(allProducts);
 
     return (
         <section className="shop">
@@ -9,7 +9,15 @@ function Shop(props) {
                 <h1>Keyboards</h1>
             </div>
             <div className="shop-display">
-
+                {allProducts.map((product) => (
+                    <div className="product-card" key={product.name} id={product.name}>
+                        <h2>{product.name}</h2>
+                        <img alt="product-card" src={product.img}></img>
+                        <div className="overlay">
+                            <button type="button">DETAILS</button>
+                        </div>
+                    </div>
+                ))}
             </div>
         </section>
     )
